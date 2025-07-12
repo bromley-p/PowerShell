b6e3cba9cec20456fd8907338c35820aa08f43d2
bromley-p <petermatthewbromley@hotmail.com>
2025-07-12 16:15:00 -0400

    Add files via upload

---
+++ CompareTwoFiles.ps1
@@ -0,0 +1,19 @@
+﻿$strReference = ""
+$strDifference = ""
+$ResultIdentical = ""
+$ResultDifferent = ""
+
+$file1 = "C:\temp\file1.txt"
+$file2 = "C:\temp\file2.txt"
+
+$strReference = Get-Content $file1
+$strDifference = Get-Content $file2
+
+Write-Host "`n****** SETTINGS ******`n`nThis is comparing the following files (you can edit the file name / location in the script)`nFile 1: $file1`nFile 2: $file2"
+
+$ResultIdentical = Compare-Object $strReference $strDifference -IncludeEqual -ExcludeDifferent -PassThru
+$ResultDifference =  Compare-Object $strReference $strDifference -PassThru
+
+$identical = "`nIdentical Content Detected:"
+$differences = "`nDifferences Detected:"
+Write-Output $identical $ResultIdentical $differences $ResultDifference
